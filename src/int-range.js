1ac957429aa2758cf49d548b8b57312ac1319690
Charles Odili <chaluwa@gmail.com>
2019-11-28 23:06:04 +0100

    support increment and decrement

--- src/int-range.js
+++ src/int-range.js
@@ -1,11 +1,30 @@
+const getUpdateStrategy = (start, limit, steps) => {
+
+  const isIncrement = start < limit;
+
+  const increment = (value) => value + steps; 
+  const decrement = (value) => value - steps;  
+
+  const canIncrement = (value) => value <= limit;
+  const canDecrement = (value) => value >= limit;
+
+  const hasNext = isIncrement === true ? canIncrement : canDecrement;
+  const next = isIncrement === true ? increment : decrement;
+
+  return {next, hasNext};
+
+};
 
 const intRange = (options = {}) => {
     const { start = 0, limit = 20, steps = 1 } = options;
 
     const range = [];
+    const {next, hasNext} = getUpdateStrategy(start, limit, steps);
 
-    for (let counter = start; counter <= limit; counter += steps) {
-        range.push(counter);
+    let value = start;
+    while( hasNext(value) ) {
+        range.push(value);
+        value = next(value);
     }
 
     return range;
