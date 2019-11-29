6f658f8269bd68b79df3653df98821cebe3c1d73
Charles Odili <chaluwa@gmail.com>
2019-11-29 05:46:46 +0100

    decouple increment and decrement from strategy function

--- src/int-range.js
+++ src/int-range.js
@@ -1,33 +1,57 @@
-const getUpdateStrategy = (start, limit, steps) => {
+const incrementBy = (steps) => (value) => value + steps; 
+const decrementBy = (steps) => (value) => value - steps;  
 
+const incrementUoTo = (limit) => (value) => value <= limit;
+const decrementUpTo = (limit) =>  (value) => value >= limit;
+
+const getUpdateStrategy = (start, limit, steps) => {
+  const strategy = {};
   const isIncrement = start < limit;
 
-  const increment = (value) => value + steps; 
-  const decrement = (value) => value - steps;  
 
-  const canIncrement = (value) => value <= limit;
-  const canDecrement = (value) => value >= limit;
+  strategy.next = isIncrement === true ? incrementBy(steps) : decrementBy(steps);
+  strategy.hasNext = isIncrement === true ? incrementUoTo(limit) : decrementUpTo(limit);
 
-  const hasNext = isIncrement === true ? canIncrement : canDecrement;
-  const next = isIncrement === true ? increment : decrement;
-
-  return {next, hasNext};
+  return strategy;
 
 };
 
-const intRange = (options = {}) => {
-    const { start = 0, limit = 20, steps = 1 } = options;
 
-    const range = [];
-    const {next, hasNext} = getUpdateStrategy(start, limit, steps);
+export const even = (gait = 1) => (updateValueByOne) => {
 
-    let value = start;
-    while( hasNext(value) ) {
-        range.push(value);
-        value = next(value);
+  const isEven = (int) => int % 2 === 0;
+
+  return (value) => {
+
+    const matches = 0;
+    let nextValue = value;
+    
+    while( matches < gait ) {
+       nextValue = updateValueByOne(nextValue);
+
+      if(isEven(nextValue)) matches += 1;
     }
 
-    return range;
+    return nextValue;
+
+  };
+
+};
+
+export const intRange = ( options = {} ) => {
+  const {start = 0, limit = 20, steps = 1} = options;
+
+  const range = [];
+
+  const {next, hasNext} = getUpdateStrategy(start, limit, steps);
+
+  let value = start;
+  while( hasNext(value) ) {
+    range.push(value);
+    value = next(value);
+  }
+
+  return range;
 };
 
 export default intRange;
