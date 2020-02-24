c88756428729afc1bc4f23bd32de5c060dd72f9c
Charles Odili <chaluwa@gmail.com>
2020-02-24 16:35:33 +0100

    expect samples run in browser

--- examples/index.js
+++ examples/index.js
@@ -2,25 +2,24 @@
 import { intRange } from '../src/int-range.js';
 import { leapYear } from '../src/plugins/leap-year.js';
 
-if (document && document.addEventListener) {
-    const displayRange = (ints) => {
-        const list = document.querySelector('#list');
-        list.innerHTML = '';
-    
-        ints.forEach(int => {
-            const listIten = document.createElement('li');
-            listIten.textContent = int;
-            list.appendChild(listIten);
-        });
-    };
-    
-    document.addEventListener('DOMContentLoaded', () => {
-        const range = intRange({
-            start: 1980,
-            limit: 2020,
-            sequence: leapYear({steps: 2})
-        });
-    
-        displayRange(range);
+const displayRange = (ints) => {
+    const list = document.querySelector('#list');
+    list.innerHTML = '';
+
+    ints.forEach(int => {
+        const listIten = document.createElement('li');
+        listIten.textContent = int;
+        list.appendChild(listIten);
     });
+};
+
+const runSamples = () => {
+    // const range = intRange({
+    //     start: 1980,
+    //     limit: 2020,
+    //     sequence: leapYear({steps: 2})
+    // });
+    // displayRange(range);
 }
+
+document.addEventListener('DOMContentLoaded', runSamples);
